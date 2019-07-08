e2010dd219a73512ca6808d38e6beaadeb20b9b3
Lotanna <pythonchal@gmail.com>
2019-07-08 00:42:26 -0700

    updated basic_utility docsting

--- utils/basic_utilities.py
+++ utils/basic_utilities.py
@@ -4,6 +4,13 @@ class Utilities:
 
     @staticmethod
     def check_subscriptability(pod):
+        """Checks if an Application Grouping is Subscriptable
+
+        :param str pod: Defines Pod objedct returned from getting deployment Pods.
+        :return: str application group
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
 
         try:
             application_group = pod.metadata.labels['applicationGroup']
