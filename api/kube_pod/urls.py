12a865ae19f74f568f18221aa40db13917cfd430
Lotanna <pythonchal@gmail.com>
2019-07-08 01:16:31 -0700

    ran django migration script

--- api/kube_pod/urls.py
+++ api/kube_pod/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from .views import *
 from rest_framework import routers
 
+
 router = routers.DefaultRouter()
 router.register(r'service', DefaultNameSpacePods, base_name='default_namespace')
 router.register(r'service/(?P<application_group>[^/.]+)', DefaultNameSpacePods, base_name='applicationGroup')
